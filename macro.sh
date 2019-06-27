3580a1e50a2f0a0047cb0f8b6cf74f2afb66b683
Andrias Hansen <s1878555@ed.ac.uk>
2019-06-27 14:18:09 +0100

    Now for two input parameters

--- macro.sh
+++ macro.sh
@@ -2,10 +2,7 @@
 
 ./compile.sh ### Compiles the executable
 
-touch data.txt  # Creates an empty txt file IF it does not exits
-echo > data.txt # Empties the txt file
-
-for i in {26..120}
+for i in {26..110}
 do
-    ./wls $i.0
+    ./wls $i.0 $i.0
 done

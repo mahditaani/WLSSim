92ad4d20f1edf64da6a3676195564c6dbed4d47b
Andrias Hansen <s1878555@ed.ac.uk>
2019-07-10 14:50:06 +0100

    Resolved a conflict

--- macro.sh
+++ macro.sh
@@ -2,13 +2,10 @@
 
 ./compile.sh ### Compiles the executable
 
-<<<<<<< HEAD
-=======
 touch data.txt  # Creates an empty txt file IF it does not exits
 echo > data.txt # Empties the txt file
 
 for ((i = 20; i <= 320; i += 10));
->>>>>>> 13bd10442debaa5ed2177701db5091614364ac5f
 do
     ./wls 50.0 50.0 $i.0
 done

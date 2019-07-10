c4a6ad9511aeb300712f9c4d8c3158a406e839fc
Andrias Hansen <s1878555@ed.ac.uk>
2019-07-10 14:38:41 +0100

    Now takes x and y values and attenuation. This particular code explores the rectangular ratio efficiency

--- dataAnalyse.py
+++ dataAnalyse.py
@@ -3,7 +3,7 @@
 #
 """
 ========================================
-Last updated Sat. 26th June 2019
+Last updated Sat. 1st July 2019
 by Andrias Hansen
 Title:
     dataAnalyse.py
@@ -16,58 +16,40 @@ Discription:
 import numpy as np
 import matplotlib.pyplot as plt
 
+QE      = 0.8   # Quantum efficiency
+QY      = 1.0   # Quantum yield
+T       = 0.4   # Transmission: plastic -> air
+U       = 1.0   # Uniformity
+L       = 0.77  # Loss from the top and bottom plate
+WLS0    = 1.0   # Unknown scaling factor
+
+eff = QE * QY * T * U * L * WLS0
+
 # Loads datafile
-parametersL20 = np.loadtxt("dataL20.txt")
+parameters = np.loadtxt("data.txt")
 
-parametersL40 = np.loadtxt("dataL40.txt")
-parametersL80 = np.loadtxt("dataL80.txt")
-parametersL160 = np.loadtxt("dataL160.txt")
-parametersL320 = np.loadtxt("dataL320.txt")
+WLSx_sizes = parameters[:,0]
+WLSy_sizes = parameters[:,1]
+ratio = parameters[:,2]
+PMT_hits = parameters[:,3]
+PhotonsGenerated = parameters[:,4]
+Attenuation = parameters[:,5]
 
-WLS_sizesL20 = parametersL20[:,0]
-PMT_hitsL20 = parametersL20[:,1]
-PhotonsGeneratedL20 = parametersL20[:,2]
+PMTArea = np.pi * 14.0 * 14.0
 
-PMT_hitsL40 = parametersL40[:,1]
-PhotonsGeneratedL40 = parametersL40[:,2]
-PMT_hitsL80 = parametersL80[:,1]
-PhotonsGeneratedL80 = parametersL80[:,2]
-PMT_hitsL160 = parametersL160[:,1]
-PhotonsGeneratedL160 = parametersL160[:,2]
-PMT_hitsL320 = parametersL320[:,1]
-PhotonsGeneratedL320 = parametersL320[:,2]
+efficiency = PMT_hits / PhotonsGenerated
+area = (WLSx_sizes * WLSy_sizes) - PMTArea
 
-PMTArea = np.pi * 12.7 * 12.7
+factorArea = area / PMTArea
 
-efficiencyL20 = PMT_hitsL20 / PhotonsGeneratedL20
-areaL20 = (WLS_sizesL20 * WLS_sizesL20) - PMTArea
+gain = 1 + factorArea * efficiency * eff
 
-efficiencyL40 = PMT_hitsL40 / PhotonsGeneratedL40
-efficiencyL80 = PMT_hitsL80 / PhotonsGeneratedL80
-efficiencyL160 = PMT_hitsL160 / PhotonsGeneratedL160
-efficiencyL320 = PMT_hitsL320 / PhotonsGeneratedL320
-
-factorArea = areaL20 / PMTArea
-
-gainL20 = 1 + factorArea * efficiencyL20
-
-gainL40 = 1 + factorArea * efficiencyL40
-gainL80 = 1 + factorArea * efficiencyL80
-gainL160 = 1 + factorArea * efficiencyL160
-gainL320 = 1 + factorArea * efficiencyL320
-
-plt.plot(WLS_sizesL20,gainL20, label = "l = 20 cm")
-
-plt.plot(WLS_sizesL20,gainL40, label = "l = 40 cm")
-plt.plot(WLS_sizesL20,gainL80, label = "l = 80 cm")
-plt.plot(WLS_sizesL20,gainL160, label = "l = 160 cm")
-plt.plot(WLS_sizesL20,gainL320, label = "l = 320 cm")
+plt.plot(ratio, efficiency)
 
 plt.grid(linestyle='dotted')
-plt.title('Gain as a function of square WLS length')
-plt.xlabel('Square WSL plate length [cm]')
-plt.ylabel("Gain")
+plt.title('Efficiency as a function ratio with a fixed area')
+plt.xlabel('Ratio')
+plt.ylabel("Efficiency")
 
-plt.legend()
 plt.show()
 

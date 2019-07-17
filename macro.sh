f9a212074f964fea84712f2ae89bf537180030fe
Andrias Hansen <s1878555@ed.ac.uk>
2019-07-17 14:19:38 +0100

    Created a manual loop for ratio study

--- macro.sh
+++ macro.sh
@@ -2,10 +2,29 @@
 
 ./compile.sh ### Compiles the executable
 
-touch data.txt  # Creates an empty txt file IF it does not exits
-echo > data.txt # Empties the txt file
+#for ((i = 30; i <= 120; i++));
+#do
+#    ./wls $i.0 $i.0 100
+#done
 
-for ((i = 50; i <= 50; i += 10));
-do
-    ./wls $i.0 $i.0 100
-done
+./wls 50.00 50.00 100
+
+./wls 49.27 50.74 100
+./wls 48.34 51.72 100
+./wls 47.46 52.68 100
+
+./wls 46.63 53.62 100
+./wls 45.64 54.77 100
+./wls 44.72 55.90 100
+
+./wls 43.85 57.01 100
+./wls 42.72 58.52 100
+./wls 41.81 59.79 100
+
+./wls 40.96 61.03 100
+./wls 39.65 63.05 100
+./wls 38.81 64.42 100
+
+./wls 37.80 66.14 100
+./wls 36.37 68.74 100
+./wls 35.36 70.71 100
